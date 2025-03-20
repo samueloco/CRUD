80348fab4e62435fa42867cbc0b324bd8453f72a
samueloco <samueladorna77@gmail.com>
2025-03-20 23:41:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User List</title>
+    <title>Student List</title>
     <style>
         body {
             font-family: Arial, sans-serif;
